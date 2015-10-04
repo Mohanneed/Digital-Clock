d91857bebd608924e93509d8def7945ab1504b8f
mac <ahmed@ahmeds-macbook-pro.local>
2015-10-04 16:57:37 +0200

    Initial Commit

---
+++ Digital Clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Digital Clock
+//
+//  Created by Ahmed Youssef Elzohry on 10/4/15.
+//  Copyright © 2015 Ahmed Elzohry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
